c1996f90a23453225815697a24da40f30f087734
Gregory.Johnson <3531op@gmail.com>
2020-11-09 09:30:57 -0500

    Added alternate function solution

--- get-category.js
+++ get-category.js
@@ -1,17 +1,19 @@
 const emojis = require('./emojis.js');
 
 
+// const getCategory = function (category) {
+//   const findCategory = emojis.filter((emoji) => {
+//     if (emoji.categories.includes(category)) {
+//       return emoji;
+//     }
+
+//   })
+//   return findCategory;
+// }
+
 const getCategory = function (category) {
-  const findCategory = emojis.filter((emoji) => {
-    if (emoji.categories.includes(category)) {
-      return emoji;
-    }
-
-  })
-  return findCategory;
+return emojis.filter((emoji) => emoji.categories.includes(category))
 }
-
-
 
 
 
